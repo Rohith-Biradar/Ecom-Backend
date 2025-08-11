68d6160877a64c0d8cf1e4b5760c47f781e31888
Rohith Biradar <153374054+Rohith-Biradar@users.noreply.github.com>
2025-08-12 00:26:48 +0530

    Update ProductController.java

--- src/main/java/com/ecom/ecomproject/controller/ProductController.java
+++ src/main/java/com/ecom/ecomproject/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "https://ecom-frontend-ten-beige.vercel.app")
 public class ProductController {
@@ -88,4 +88,5 @@ public class ProductController {
     }
 
 }
+
 
